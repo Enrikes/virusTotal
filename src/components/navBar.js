87b19baf9a0b641c048720681905c4013f0b69f8
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2025-02-12 08:36:49 -0800

    Initial commit.

---
+++ src/components/navBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./cssModules/navbar.modules.css";
+
+const Navbar = () => (
+  <nav className={styles.navbar}>
+    <h1 className={styles.title}>EnriqueTotal</h1>
+  </nav>
+);
+
+export default Navbar;
